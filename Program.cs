7bc9ceec5ec4407d5cd317833533b51b03833bc8
ingache <ingache@gmail.com>
2023-12-03 22:02:28 +0100

    Vault Integration

--- Program.cs
+++ Program.cs
@@ -1,15 +1,59 @@
 ﻿using AsymmetricEncryptionNet.Services;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Azure.KeyVault;
 
-RsaKeyGenerator rsaKeyGenerator = new();
+bool enableVault = false;
 
-JwtTokenManager jWTTokenManager = new(rsaKeyGenerator.GetPrivateKeyPEM());
+if (enableVault)
+{
+    string clientId = "SOME_CLIENT_ID";
+    string clientSecret = "SOME_CLIENT_SECRET";
+    string tenantId = "SOME_TENANT";
+    string keyVaultUrl = "SOME_VAULT_ENDPOINT";
+
+    var publicKeySecretName = "JwtPublicKeyPem";
+    var privateKeySecretName = "JwtPrivateKeyPem";
+
+    var client = new SecretClient(
+        new Uri(keyVaultUrl),
+        new ClientSecretCredential(tenantId, clientId, clientSecret));
+
+    KeyVaultSecret publicSecret = client.GetSecret(publicKeySecretName);
+
+    KeyVaultSecret privateSecret = client.GetSecret(privateKeySecretName);
+
+    JwtTokenManager jWTTokenManager = new(privateSecret.Value);
+
+    Console.WriteLine("Token: " + jWTTokenManager.Token);
+
+    Console.WriteLine("Token plain text: " + jWTTokenManager.DecodeToken());
+
+    Console.WriteLine("Token is valid: " + jWTTokenManager.ValidateToken(publicSecret.Value));
+}
+else {
+    RsaKeyGenerator rsaKeyGenerator = new();
+
+    JwtTokenManager jWTTokenManager = new(rsaKeyGenerator.GetPrivateKeyPEM());
+
+    Console.WriteLine("Token: " + jWTTokenManager.Token);
+
+    Console.WriteLine("Token plain text: " + jWTTokenManager.DecodeToken());
+
+    Console.WriteLine("Private key: " + rsaKeyGenerator.GetPrivateKeyPEM());
+
+    Console.WriteLine("Public key: " + rsaKeyGenerator.GetPublicKeyPem());
+
+    Console.WriteLine("Token is valid: " + jWTTokenManager.ValidateToken(rsaKeyGenerator.GetPublicKeyPem()));
+}
 
 
 
-Console.WriteLine("Token: " + jWTTokenManager.Token);
 
-Console.WriteLine("Token plain text: " + jWTTokenManager.DecodeToken());  
 
-Console.WriteLine("Public key: " + rsaKeyGenerator.GetPublicKeyPem());
 
-Console.WriteLine("Token is valid: " + jWTTokenManager.ValidateToken(rsaKeyGenerator.GetPublicKeyPem()));
+
+
+
+
+
